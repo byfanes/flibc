feec4ce90318788c4cd15d203aafde7859d3366a
byfanes <sabazmenderes@proton.me>
2026-05-06 20:26:44 +0300

    small fix

--- src/stdio/fputc.c
+++ src/stdio/fputc.c
@@ -4,6 +4,8 @@ fc_error_t fputc
 (file_t* file, uint8_t c)
 {
     /* Make a little slice and pass it to fwrite function which takes care of everything. */
-    slice_t sl = { &c, 1};
+    slice_t sl = {0};
+    sl.base = &c;
+    __set_slice_count(sl, 1);
     return fwrite(file, sl);
 }
