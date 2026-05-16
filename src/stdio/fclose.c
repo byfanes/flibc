4e8eb13e377f56edf7b17593ac80b56f31494f49
byfanes <sabazmenderes@proton.me>
2026-05-16 12:54:44 +0300

    stdio: fclose checks

--- src/stdio/fclose.c
+++ src/stdio/fclose.c
@@ -12,14 +12,19 @@ fc_error_t fclose
     if(!*file) { return fce_success; }
 
     /* Call flush to write the remaing buffer */
-    res = fflush(*file);
-    if(res) { return res; }
+    if((*file)->type != file_read) {
+        res = fflush(*file);
+        if(res) { return res; }
+    }
 
-    /* Call close syscall */
-    ret = syscall_1(syscall_close, (*file)->fd);
+    if(!((*file)-> fd == UNIX_STDERR || (*file)-> fd == UNIX_STDIN || (*file)-> fd == UNIX_STDOUT))
+    {
+        /* Call close syscall */
+        ret = syscall_1(syscall_close, (*file)->fd);
 
-    /* Check return of the syscall */
-    if(ret != 0) { return fce_io_error; }
+        /* Check return of the syscall */
+        if(ret != 0) { return fce_io_error; }
+    }
 
     /* Free the memory back zeroed by the free */
     res = free(file);
