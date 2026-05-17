7175586a74a0d5f7e9b55ac53905acf90130a8e9
byfanes <sabazmenderes@proton.me>
2026-05-17 12:43:31 +0300

    string: buf fix validation and make it better

--- src/string/sl_utf8len.c
+++ src/string/sl_utf8len.c
@@ -6,16 +6,18 @@ error_t sl_utf8len
     /* Init variables */
     usize_t i = 0, count = 0;
     u8 c = 0;
+    u32 codepoint = 0;
 
     /* Validate user input */
     if(!out) { return null_pointer; }
     if(!sl.base || !sl.count) { *out = 0; return success; }
 
-    /* Last byte of sequence are incremented with the loop
+    /* Last byte of each sequence increments the loop
      * other ones should be handled in the flow
      */
     for(;i < sl.count; ++i, ++count) {
         c = sl.base[i];
+        codepoint = 0;
         /* Ascii 0aaa bbbb */
         if(!(c >> 7)) { continue; }
 
@@ -23,10 +25,15 @@ error_t sl_utf8len
         if((c >> 5) == 6) {
             /* Check and fetch the new byte */
             if(i + 1 >= sl.count) { goto invalid; }
+            codepoint = ((u32)(c & 0x1F) << 6);
             c = sl.base[++i];
 
             /* New byte should be 10aa bbbb (second) 2 = 0b10 */
             if((c >> 6) != 2) { goto invalid; }
+            codepoint |= ((u32)(c & 0x3F));
+
+            /* overlong */
+            if(codepoint < 0x80) { goto invalid; }
             continue;
         }
 
@@ -34,35 +41,52 @@ error_t sl_utf8len
         if((c >> 4) == 14) {
             /* Check and fetch the new byte */
             if(i + 2 >= sl.count) { goto invalid; }
+            codepoint = ((u32)(c & 0x0F) << 12);
             c = sl.base[++i];
 
             /* New byte should be 10aa bbbb (second) 2 = 0b10 */
             if((c >> 6) != 2) { goto invalid; }
+            codepoint |= ((u32)(c & 0x3F) << 6);
             c = sl.base[++i];
 
             /* New byte should be 10aa bbbb (third) 2 = 0b10 */
             if((c >> 6) != 2) { goto invalid; }
+            codepoint |= ((u32)(c & 0x3F));
 
+            /* overlong */
+            if(codepoint < 0x800) { goto invalid; }
+
+            /* surrogate */
+            if(codepoint >= 0xD800 && codepoint <= 0xDFFF) { goto invalid; }
             continue;
         }
 
-        /* 4 Byte 1111 0aaa (first) 14 = 0b1110 */
-        if((c >> 4) == 14) {
+        /* 4 Byte 1111 0aaa (first) 30 = 0b11110 */
+        if((c >> 3) == 30) {
             /* Check and fetch the new byte */
             if(i + 3 >= sl.count) { goto invalid; }
+            codepoint = ((u32)(c & 0x0F) << 18);
             c = sl.base[++i];
 
             /* New byte should be 10aa bbbb (second) 2 = 0b10 */
             if((c >> 6) != 2) { goto invalid; }
+            codepoint |= ((u32)(c & 0x3F) << 12);
             c = sl.base[++i];
 
             /* New byte should be 10aa bbbb (third) 2 = 0b10 */
             if((c >> 6) != 2) { goto invalid; }
+            codepoint |= ((u32)(c & 0x3F) << 6);
             c = sl.base[++i];
 
             /* New byte should be 10aa bbbb (fourth) 2 = 0b10 */
             if((c >> 6) != 2) { goto invalid; }
+            codepoint |= ((u32)(c & 0x3F));
 
+            /* overlong */
+            if(codepoint < 0x10000) { goto invalid; }
+
+            /* Max limit */
+            if(codepoint > 0x10FFFF) { goto invalid; }
             continue;
         }
 
