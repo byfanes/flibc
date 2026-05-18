f7c6b5b18929ea23de250b387a3ca87a65dbae2d
byfanes <sabazmenderes@proton.me>
2026-05-18 19:17:43 +0300

    Readme and some examples

---
+++ README.md
@@ -0,0 +1,19 @@
+# flibc
+
+A custom C library to fix C's historical flaws. Written in C89 without relying compiler-specific extensions and (trying to) avoiding macro-slop.
+
+> **Note:** This library is a work in progress and currently on supports x86_64 Linux.
+
+## Why does it exist?
+
+> **No Hidden/Gobal State:** The standard libc forces users to rely on hidden global state (like 'errno' or 'stdin'), which can be painful and unsafe to debug/work while using multithreading.
+
+> **Builtin Data Structures:** Every C developer eventually implements their own slice or dynamic array. By providing these basic types directly in the library, 'flibc' speeds up development and prevents reinventing the wheel.
+
+> **Explicit Error Handling:** IF there is an error, functions just return an 'error_t' instead of crashing or modifying a global variable.
+
+> **A Modern C Runtime:** The C runtime parses arguments into slices. Your program receives explicit parameters for standard stream, the executable name, cli arguments and the standard allocator.
+
+## Example Usage
+
+You can find some examples in 'examples/' folder.
---
+++ examples/da.c
@@ -0,0 +1,78 @@
+#include "crt.h"
+#include "da.h"
+#include "string.h"
+
+can_be_da(str_t);
+error_t write_list(file_t* fptr, da(str_t)* list);
+
+
+error_t write_list
+(file_t* fptr, da(str_t)* list)
+{
+    error_t res = success;
+    u32 i = 0;
+
+    for(; i < list->count; ++i) {
+        str_add_shadow_null(&list->items[i]);
+
+        if((res = fprintf(fptr, cstr_to_u8sl("List element[%d]: %s\n"), i, list->items[i].items)))
+        { return res; }
+
+        if((fflush(fptr))) { return res; }
+    }
+
+    return res;
+}
+
+error_t main
+(std_t std)
+{
+    error_t res = success;
+    u32 read_count = 0;
+    u8 buf[128] = {0};
+    slice(u8) buf_sl = {0};
+    slice(u8) write_sl = {0};
+    da(str_t) list = {0};
+    str_t tmp = {0};
+    u32 i = 0;
+
+    /* Set the buffer slice */
+    set_slice(&buf_sl, buf, sizeof(buf));
+
+    /* Set da */
+    if((res = da_init(std.alloc, &list, 32))) { return res; }
+
+    for(;;) {
+        res = fread(std.io.in, buf_sl, &read_count);
+        if(res && res != io_partial) { return res; }
+
+        /* If we see an '!' just stop the program */
+        if(buf_sl.base[0] == '!') { break; }
+
+        /* If we see an '?' just write the list */
+        if(buf_sl.base[0] == '?') {
+            if((res = write_list(std.io.out,  &list))) { return res; }
+            continue;
+        }
+
+        /* Add new item to list -1 is for removing new line*/
+        set_slice(&write_sl, buf, read_count - 1);
+        if((res = str_init(std.alloc, &tmp, read_count))) { return res; }
+        if((res = strcat_sl(&tmp, write_sl))) { return res; }
+        if((res = da_insert(&list, &tmp, 0))) { return res; }
+
+    }
+
+    /* Free memory back */
+    /* If you want to test memory leak system you can switch
+     * from true to false it will leak memory and will
+     */
+    if (true) {
+        for(; i < list.count; ++i) {
+            str_deinit(&list.items[i]);
+        }
+        da_deinit(&list);
+    }
+
+    return res;
+}
