60b7ec03b7dab77de71223a2f7791c4c87f49d4c
byfanes <sabazmenderes@proton.me>
2026-05-20 08:23:19 +0300

    stdio: O_CLOEXEC flag and forgetten close syscall

--- src/stdio/fopen.c
+++ src/stdio/fopen.c
@@ -5,31 +5,37 @@ error_t fopen
 {
     /* Init variables */
     error_t res = success;
-    ssize_t fd = 0, flags = 0;
+    ssize_t fd = 0, flags = O_CLOEXEC;
 
     /* Validate user inputs */
     if(!alloc || !name || !out) { return null_pointer; }
 
     /* Set flags */
     switch(type) {
-        case file_read: { flags = O_RDONLY; } break;
-        case file_write: { flags = O_WRONLY | O_CREAT | O_TRUNC; } break;
-        case file_append: { flags = O_WRONLY | O_CREAT | O_APPEND; } break;
-        case file_read_plus: { flags = O_RDWR; } break;
-        case file_write_plus: { flags = O_RDWR | O_CREAT | O_TRUNC; } break;
-        case file_append_plus: { flags = O_RDWR | O_CREAT | O_APPEND; } break;
+        case file_read: { flags |= O_RDONLY; } break;
+        case file_write: { flags |= O_WRONLY | O_CREAT | O_TRUNC; } break;
+        case file_append: { flags |= O_WRONLY | O_CREAT | O_APPEND; } break;
+        case file_read_plus: { flags |= O_RDWR; } break;
+        case file_write_plus: { flags |= O_RDWR | O_CREAT | O_TRUNC; } break;
+        case file_append_plus: { flags |= O_RDWR | O_CREAT | O_APPEND; } break;
         default: { return invalid_argument; }
     }
 
     /* Call read syscall */
-    fd = syscall_3_linux(syscall_open, (ssize_t)name, flags,
-    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    fd = syscall_3_linux(syscall_open, (ssize_t)name, flags, 0666);
 
     /* Check return of the syscall */
     if(fd < 0) { return io_error; }
 
     /* Allocate new memory for the struct */
-    if((res = malloc(alloc, sizeof(file_t), out))) { return res; }
+    if((res = malloc(alloc, sizeof(file_t), out))) {
+        /* We dont check this syscall because functions has already failed
+         * and close will be handled via kernel if open returns a succes
+         * closing should not be a problem
+         */
+        syscall_1_linux(syscall_close, fd);
+        return res;
+    }
 
     /* Set variables */
     (*out)->fd = fd;
