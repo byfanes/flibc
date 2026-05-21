94ac66ccee788b2a7b039cba1b6438273157e108
byfanes <sabazmenderes@proton.me>
2026-05-21 22:04:53 +0300

    mem: wrong type used in memory calculation

--- src/memory/validate_header.c
+++ src/memory/validate_header.c
@@ -9,10 +9,10 @@ bool __validate_header
     if((header->raw_alloced & (CHUNK_SIZE - 1)) != 0)
     { return false; }
 
-    /* align64(sum of req_alloc + header + u16(last_null)) should be
+    /* align64(sum of req_alloc + header + usize_t(last_null)) should be
      * equal to raw because its calculated like that
      */
-    if(ALIGN_64(header->req_alloced + sizeof(heap_header_t) + sizeof(u16)) != header->raw_alloced)
+    if(ALIGN_64(header->req_alloced + sizeof(heap_header_t) + sizeof(usize_t)) != header->raw_alloced)
     { return false; }
 
     if(header->first_null != 0)
