76da16ddf9605c8af7e5a5a92f60b82060be2fa8
byfanes <sabazmenderes@proton.me>
2026-07-29 18:35:43 +0300

    require: basic require macros added

---
+++ include/require.h
@@ -0,0 +1,31 @@
+#ifndef __FLIBC_REQUIRE_H__
+#define __FLIBC_REQUIRE_H__
+
+/* Do not put macros to group things such as
+ * (void)sizeof(x) to #define comp_check(x)
+ * it will be bad for compiler errors messages
+ */
+
+/* This header allow user to check mostly void* for compatibility for the types
+ * and being generic at same time and those checks are done in compile time and not
+ * effecting the run time
+ */
+
+/* Direct ptr checks */
+#define require_defereable(x) ((void)sizeof(*(x)))
+#define require_typed_ptr(x, type) (void)sizeof((x) - ((type *)0))
+#define require_writable_ptr(x) ((void)sizeof(*(x) = *(x)))
+#define require_member_ptr(x, member) ((void)sizeof((x)->member))
+#define require_same_ptr(x, y) ((void)sizeof((x) - (y)))
+
+/* Value type checks */
+#define require_comparable(x, y) ((void)sizeof((x) == (y)))
+#define require_member(x, member) ((void)sizeof((x).member))
+#define require_integer(x) ((void)sizeof(~(x)))
+#define require_arithmetic(x) ((void)sizeof((x) * 1))
+#define require_lvalue(x) ((void)sizeof((x) = (x)))
+#define require_scalar(x) ((void)sizeof((x) ? 1 : 0))
+#define require_orderable(x, y) ((void)sizeof((x) < (y)))
+#define require_addressable(x) ((void)sizeof(&(x)))
+
+#endif /* __FLIBC_REQUIRE_H__ */
