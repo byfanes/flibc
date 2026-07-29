934480ab7d963127f0be35d6a46bc8acd15b648e
byfanes <sabazmenderes@proton.me>
2026-07-29 18:36:33 +0300

    da: added require checks

--- include/da.h
+++ include/da.h
@@ -7,6 +7,7 @@ extern "C" {
 
 #include "error.h"
 #include "base.h"
+#include "require.h"
 #include "memory.h"
 
 /* DA - Implementation
@@ -14,6 +15,15 @@ extern "C" {
  * IT IS NOT SAFE FOR COPYING instead use pointers to dedicated struct
  * which costs less bytes to pass as an argument
  */
+
+#define require_da_type(da) ((void)sizeof( \
+require_defereable((da)->items), \
+require_integer((da)->count), \
+require_integer((da)->capacity), \
+require_addressable((da)->count), \
+require_addressable((da)->capacity), \
+require_orderable((da)->capacity, (da)->count), \
+0))
 
 #define can_be_da(type, name)   \
     typedef struct {            \
@@ -58,46 +68,67 @@ error_t da_clear(void* da);
  * underlying buffer has enough space for the shadow null segment
  */
 error_t __da_add_shadow_null_segment(void* da, usz el_size);
-#define da_add_shadow_null_segment(da) __da_add_shadow_null_segment(ptr_meta((da)))
+#define da_add_shadow_null_segment(da) \
+(require_da_type((da)), __da_add_shadow_null_segment((da), sizeof((da)->items[0])))
 
 error_t __da_init(allocator_t* alloc, void* da, usz el_size, usz amount);
-#define da_init(allocator, da, amount) __da_init((allocator), ptr_meta((da)), (amount))
+#define da_init(allocator, da, amount) \
+(require_da_type((da)), __da_init((allocator), (da), sizeof((da)->items[0]), (amount)))
 
 error_t __da_grow(void* da, usz el_size, usz amount);
-#define da_grow(da, amount) __da_grow(ptr_meta((da)), (amount))
+#define da_grow(da, amount) \
+(require_da_type((da)), __da_grow((da), sizeof((da)->items[0]), (amount)))
 
 error_t __da_grow_if(void* da, usz el_size, usz amount);
-#define da_grow_if(da, amount) __da_grow_if(ptr_meta((da)), (amount))
+#define da_grow_if(da, amount) \
+(require_da_type((da)), __da_grow_if((da), sizeof((da)->items[0]), (amount)))
 
 error_t __da_unordered_remove(void* da, usz el_size, usz idx);
-#define da_unordered_remove(da, idx)  __da_unordered_remove(ptr_meta((da)), (idx))
+#define da_unordered_remove(da, idx) \
+(require_da_type((da)), __da_unordered_remove((da), sizeof((da)->items[0]), (idx)))
 
 error_t __da_unordered_remove_on_ptr(void* da, usz el_size, void *ptr);
-#define da_unordered_remove_on_ptr(da, ptr)  __da_unordered_remove_on_ptr(ptr_meta_check((da), (ptr)), (ptr))
+#define da_unordered_remove_on_ptr(da, ptr) \
+(require_da_type((da)), require_same_ptr((da)->items, (ptr)), \
+ __da_unordered_remove_on_ptr((da), sizeof((da)->items[0]), (ptr)))
 
 error_t __da_swap(void* da, usz el_size, usz lhs, usz rhs);
-#define da_swap(da, lhs, rhs) __da_swap(ptr_meta((da)), (lhs), (rhs))
+#define da_swap(da, lhs, rhs) \
+(require_da_type((da)), __da_swap((da), sizeof((da)->items[0]), (lhs), (rhs)))
 
 error_t __da_push(void* da, usz el_size, void* item);
-#define da_push(da, item) __da_push(ptr_meta_check((da), item), (item))
+#define da_push(da, item) \
+(require_da_type((da)), require_same_ptr((da)->items, item), \
+ __da_push((da), sizeof((da)->items[0]), (item)))
 
 error_t __da_push_sl(void* da, usz el_size, void* sl);
-#define da_push_sl(da, sl) __da_push_sl(ptr_meta_check((da), (sl)->items), (sl))
+#define da_push_sl(da, sl) \
+(require_da_type((da)), require_sl_type((sl)), \
+__da_push_sl((da), sizeof((da)->items[0]), (sl)))
 
 error_t __da_insert(void* da, usz el_size, void* in, usz idx);
-#define da_insert(da, in, idx) __da_insert(ptr_meta_check((da), (in)), (in), (idx))
+#define da_insert(da, in, idx) \
+(require_da_type((da)), require_same_ptr((da)->items, in), \
+ __da_insert((da), sizeof((da)->items[0]), (in), (idx)))
 
 error_t __da_remove(void* da, usz el_size, usz idx);
-#define da_remove(da, idx) __da_remove(ptr_meta((da)), (idx))
+#define da_remove(da, idx) \
+(require_da_type((da)), __da_remove((da), sizeof((da)->items[0]), (idx)))
 
 error_t __da_pop(void* da, usz el_size, void* out, usz idx);
-#define da_pop(da, out, idx) __da_pop(ptr_meta_check((da), (out)), (out), (idx))
+#define da_pop(da, out, idx) \
+(require_da_type((da)), require_same_ptr((da)->items, out), \
+ __da_pop((da), sizeof((da)->items[0]), (out), (idx)))
 
 error_t __da_pop_first(void* da, usz el_size, void* out);
-#define da_pop_first(da, out) __da_pop_first(ptr_meta_check((da), (out)), (out))
+#define da_pop_first(da, out) \
+(require_da_type((da)), require_same_ptr((da)->items, out), \
+ __da_pop_first((da), sizeof((da)->items[0]), (out), (out)))
 
 error_t __da_pop_last(void* da, usz el_size, void* out);
-#define da_pop_last(da, out) __da_pop_last(ptr_meta_check((da), (out)), (out))
+#define da_pop_last(da, out) \
+(require_da_type((da)), require_same_ptr((da)->items, out), \
+ __da_pop_last((da), sizeof((da)->items[0]), (out)))
 
 #ifdef __cplusplus
 }
