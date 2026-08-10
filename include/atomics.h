20fbad95502b67e9327c142868402f7535e28d00
byfanes <sabazmenderes@proton.me>
2026-08-10 09:31:23 +0300

    atomics: update the types

--- include/atomics.h
+++ include/atomics.h
@@ -12,36 +12,36 @@ extern "C" {
 bool atomic_cas_u32(volatile u32* ptr, u32 expected, u32 new_val);
 bool atomic_cas_u64(volatile u64* ptr, u64 expected, u64 new_val);
 bool atomic_cas_usz(volatile usz* ptr, usz expected, usz new_val);
-bool atomic_cas_i32(volatile i32* ptr, i32 expected, i32 new_val);
-bool atomic_cas_i64(volatile i64* ptr, i64 expected, i64 new_val);
+bool atomic_cas_s32(volatile s32* ptr, s32 expected, s32 new_val);
+bool atomic_cas_s64(volatile s64* ptr, s64 expected, s64 new_val);
 bool atomic_cas_ssz(volatile ssz* ptr, ssz expected, ssz new_val);
 
 u32 atomic_load_u32(volatile u32* ptr);
 u64 atomic_load_u64(volatile u64* ptr);
 usz atomic_load_usz(volatile usz* ptr);
-i32 atomic_load_i32(volatile i32* ptr);
-i64 atomic_load_i64(volatile i64* ptr);
+s32 atomic_load_s32(volatile s32* ptr);
+s64 atomic_load_s64(volatile s64* ptr);
 ssz atomic_load_ssz(volatile ssz* ptr);
 
 u32 atomic_xchg_u32(volatile u32* ptr, u32 new_val);
 u64 atomic_xchg_u64(volatile u64* ptr, u64 new_val);
 usz atomic_xchg_usz(volatile usz* ptr, usz new_val);
-i32 atomic_xchg_i32(volatile i32* ptr, i32 new_val);
-i64 atomic_xchg_i64(volatile i64* ptr, i64 new_val);
+s32 atomic_xchg_s32(volatile s32* ptr, s32 new_val);
+s64 atomic_xchg_s64(volatile s64* ptr, s64 new_val);
 ssz atomic_xchg_ssz(volatile ssz* ptr, ssz new_val);
 
 u32 atomic_fetch_add_u32(volatile u32* ptr, u32 val);
 u64 atomic_fetch_add_u64(volatile u64* ptr, u64 val);
 usz atomic_fetch_add_usz(volatile usz* ptr, usz val);
-i32 atomic_fetch_add_i32(volatile i32* ptr, i32 val);
-i64 atomic_fetch_add_i64(volatile i64* ptr, i64 val);
+s32 atomic_fetch_add_s32(volatile s32* ptr, s32 val);
+s64 atomic_fetch_add_s64(volatile s64* ptr, s64 val);
 ssz atomic_fetch_add_ssz(volatile ssz* ptr, ssz val);
 
 u32 atomic_fetch_sub_u32(volatile u32* ptr, u32 val);
 u64 atomic_fetch_sub_u64(volatile u64* ptr, u64 val);
 usz atomic_fetch_sub_usz(volatile usz* ptr, usz val);
-i32 atomic_fetch_sub_i32(volatile i32* ptr, i32 val);
-i64 atomic_fetch_sub_i64(volatile i64* ptr, i64 val);
+s32 atomic_fetch_sub_s32(volatile s32* ptr, s32 val);
+s64 atomic_fetch_sub_s64(volatile s64* ptr, s64 val);
 ssz atomic_fetch_sub_ssz(volatile ssz* ptr, ssz val);
 
 void atomic_memory_barrier(void);
--- src/arch/x86_64/atomics.s
+++ src/arch/x86_64/atomics.s
@@ -4,32 +4,32 @@
 .global atomic_cas_u32
 .global atomic_cas_u64
 .global atomic_cas_usz
-.global atomic_cas_i32
-.global atomic_cas_i64
+.global atomic_cas_s32
+.global atomic_cas_s64
 .global atomic_cas_ssz
 .global atomic_load_u32
 .global atomic_load_u64
 .global atomic_load_usz
-.global atomic_load_i32
-.global atomic_load_i64
+.global atomic_load_s32
+.global atomic_load_s64
 .global atomic_load_ssz
 .global atomic_xchg_u32
 .global atomic_xchg_u64
 .global atomic_xchg_usz
-.global atomic_xchg_i32
-.global atomic_xchg_i64
+.global atomic_xchg_s32
+.global atomic_xchg_s64
 .global atomic_xchg_ssz
 .global atomic_fetch_add_u32
 .global atomic_fetch_add_u64
 .global atomic_fetch_add_usz
-.global atomic_fetch_add_i32
-.global atomic_fetch_add_i64
+.global atomic_fetch_add_s32
+.global atomic_fetch_add_s64
 .global atomic_fetch_add_ssz
 .global atomic_fetch_sub_u32
 .global atomic_fetch_sub_u64
 .global atomic_fetch_sub_usz
-.global atomic_fetch_sub_i32
-.global atomic_fetch_sub_i64
+.global atomic_fetch_sub_s32
+.global atomic_fetch_sub_s64
 .global atomic_fetch_sub_ssz
 .global atomic_memory_barrier
 
@@ -38,7 +38,7 @@ atomic_memory_barrier:
         ret
 
 atomic_cas_u32:
-atomic_cas_i32:
+atomic_cas_s32:
         mov     eax, esi
         lock cmpxchg dword ptr [rdi], edx
         sete    al
@@ -48,7 +48,7 @@ atomic_cas_i32:
 atomic_cas_usz:
 atomic_cas_ssz:
 atomic_cas_u64:
-atomic_cas_i64:
+atomic_cas_s64:
         mov     rax, rsi
         lock cmpxchg qword ptr [rdi], rdx
         sete    al
@@ -56,19 +56,19 @@ atomic_cas_i64:
         ret
 
 atomic_load_u32:
-atomic_load_i32:
+atomic_load_s32:
         mov     eax, dword ptr [rdi]
         ret
 
 atomic_load_usz:
 atomic_load_ssz:
 atomic_load_u64:
-atomic_load_i64:
+atomic_load_s64:
         mov     rax, qword ptr [rdi]
         ret
 
 atomic_xchg_u32:
-atomic_xchg_i32:
+atomic_xchg_s32:
         mov     eax, esi
         xchg    eax, dword ptr [rdi]
         ret
@@ -76,13 +76,13 @@ atomic_xchg_i32:
 atomic_xchg_usz:
 atomic_xchg_ssz:
 atomic_xchg_u64:
-atomic_xchg_i64:
+atomic_xchg_s64:
         mov     rax, rsi
         xchg    rax, qword ptr [rdi]
         ret
 
 atomic_fetch_add_u32:
-atomic_fetch_add_i32:
+atomic_fetch_add_s32:
         mov     eax, esi
         lock xadd dword ptr [rdi], eax
         ret
@@ -90,13 +90,13 @@ atomic_fetch_add_i32:
 atomic_fetch_add_usz:
 atomic_fetch_add_ssz:
 atomic_fetch_add_u64:
-atomic_fetch_add_i64:
+atomic_fetch_add_s64:
         mov     rax, rsi
         lock xadd qword ptr [rdi], rax
         ret
 
 atomic_fetch_sub_u32:
-atomic_fetch_sub_i32:
+atomic_fetch_sub_s32:
         mov     eax, esi
         neg     eax
         lock xadd dword ptr [rdi], eax
@@ -105,7 +105,7 @@ atomic_fetch_sub_i32:
 atomic_fetch_sub_usz:
 atomic_fetch_sub_ssz:
 atomic_fetch_sub_u64:
-atomic_fetch_sub_i64:
+atomic_fetch_sub_s64:
         mov     rax, rsi
         neg     rax
         lock xadd qword ptr [rdi], rax
